db81ad82106a649461d61e7f45f48282e02164f9
cf9874 <cf9874@gmail.com>
2025-08-20 05:15:43 +0900

    var, String

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,13 @@
+// import 'dart:developer';
 
+import 'dart:developer';
+
+void main() {
+  log("asdasd");
+  var name = 'fe';
+  print(name);
+  String nick = 'iron';
+  print(nick);
+  int test = 1;
+
+}
