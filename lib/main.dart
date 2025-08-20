7ef8a56a95825143117cbc936d78cf5aa55b7773
cf9874 <cf9874@gmail.com>
2025-08-21 03:22:37 +0900

    late variables

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,20 @@
 import 'dart:developer';
 
 void main() {
-  // Final Variables
-  //  한번 정의된 변수를 수정할 수 없게 만드는 방법
-
-  String nick = 'fe';
-  nick = 'iron';
-
-  final name = 'gang';
-  name = '11'; // wrong usage
-  // The final variable 'name' can only be set once.
-  // as const in JS
-  final bool gender = true;
+  // late is a modifier(수식어)
+  // And it should be placed before type like fianl, var, String...
+  // usage
+  // late는 초기 데이터 없이 변수를 선언할 수 있게 해준다.
+  // like let in JS
+  late final String name;
+  // do something, go to api
+  var resultByApi = '1234'; // result by api
+  print(name);
+  //The late local variable 'name' is definitely unassigned at this point.
+  // 아직 name에 값이 없기 떄문에 name에 접근 불가
+  name = resultByApi;
+  print(name)
+  //final 변수를 사용하고 싶은데 그 값을 서버에서 받아야 한다면 late 사용
+  // if you want to use final variables with result that it could be got from server,
+  // you can use late
 }
