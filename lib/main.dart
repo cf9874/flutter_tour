d04452860acedd4b718c3f7e37d9e4c03ff600de
cf9874 <cf9874@gmail.com>
2025-08-21 03:44:05 +0900

    basic data type

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,15 @@
 import 'dart:developer';
 
 void main() {
-  // const in Dart is defferent in JS or TS
-  // const in JS or TS is simillar with fianl in Dart
+  // All data types in dart is Object. even func is also object.
+  String name = "name";
+  bool alive = true;
+  int age = 31;
+  double money = 69.99;
+  // these type are class.(object)
 
-  // dart에서 const는 compile-time constat를 만들어준다
-
-  // compile-time constat
-  const name = 'gang';
-  // final처럼 이후 수정불가
-  // const는 compile-time에 알고 있는 값이어야한다는 것
-  // const는 컴파일 할 떄 알고 있는 값에 사용한다라 ..
-  // 앱스토어에 앱을 올리기전에 알고 있는 값..
-  // 어떤 값인지 모르고, 그 값이 API로 부터 온다거나,
-  // 사용자가 입력해야 하는 값이라면 const를 사용하지 않음 (그런건 var or final)
-  const max_allowed_price = 1200000;
-  // API_KEY 같은 값 사용하는거네
-  // 이런 값은 컴파일 할 떄에 평가된다.
-  // => 앱에 담긴 코드를 앱스토어에 보내기 전에!
+  // but
+  num x = 12;
+  x = 1.1;
+  // so these data type have a lot of methods!
 }
