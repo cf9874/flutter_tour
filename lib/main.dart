c8ad1db5dc8bc5ee545612a664927b70a73b9fb4
cf9874 <cf9874@gmail.com>
2025-08-25 03:18:07 +0900

    Map

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,30 @@
 import 'dart:developer';
 
 void main() {
-  var oldFriends = [
-    'dh','js',
-  ];
-  var newFriends=[
-    'gh',
-    'is',
-    for (var friend in oldFriends) '$friend ❤️'
-  ];
-  print(newFriends);
+
+
+  var charactor = {
+    "id":"cf9874",
+    "age":31,
+  };
+  // Map<String, Object> charactor
+
+  print(charactor);
+  
+  Map<int,bool> example ={
+    1:false,
+    2:true,
+    3:true,
+    4:false,
+    // 'st':"Asd",
+    // The element type 'String' can't be assigned to the map value type 'bool
+  };
+  print(example);
+
+  Map<List<int>,bool> variation = {
+    [1,2,3]:false,
+    [2,3,5]:true,
+  };
+
+  print(variation);
 }
